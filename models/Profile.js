0b16254298fedbc8ec2e77738e7cf7bdddf4867a
Aman <gattiflab@gmail.com>
2020-08-20 22:28:04 +0530

    Profile Schema

---
+++ models/Profile.js
@@ -0,0 +1,52 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const ProfileSchema = new Schema({
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: "users",
+  },
+  displayName: {
+    type: String,
+    requied: true,
+  },
+  country: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+  },
+  totalGamesPlayed: {
+    type: Number,
+  },
+  favGenre: {
+    type: String,
+  },
+  social: {
+    youtube: {
+      type: String,
+    },
+    discord: {
+      type: String,
+    },
+    twitch: {
+      type: String,
+    },
+    steam: {
+      type: String,
+    },
+    facebook: {
+      type: String,
+    },
+    instagram: {
+      type: String,
+    },
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = Profile = mongoose.model("profile", ProfileSchema);
